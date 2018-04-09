0729b3858bb248b1480a95c220f51c2279d5ddd3
Joffrey Janiec <jjaniec@student.42.fr>
2018-04-09 21:19:58 +0200

    add support for 'x is a directory' / 'x: permission denied'

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/05 14:53:50 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/04/09 20:24:41 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/04/09 20:57:11 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,5 +81,9 @@ unsigned int				ft_wait_pid(pid_t pid);
 int							ft_can_exec_path(t_msh_command *cmd, char *path);
 
 void						ft_err_cmd_not_found(t_msh_command *cmd);
+
+void						ft_handle_err_code(int err, char *prog_name);
+
+int							ft_handle_err(t_msh_command *cmd);
 
 #endif
--- srcs/ft_handle_err.c
+++ srcs/ft_handle_err.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 20:18:44 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/04/09 20:29:13 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/04/09 21:18:30 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 ** https://github.com/hkbakke/bash-insulter
 */
 
-void	ft_print_err_head(char *s)
+static void	ft_print_err_head(char *s)
 {
 	ft_putstr_fd("\n   \e[91m", 2);
 	ft_putstr_fd(s, 2);
@@ -28,10 +28,36 @@ void	ft_print_err_head(char *s)
 ** Prints command not found error message
 */
 
-void	ft_err_cmd_not_found(t_msh_command *cmd)
+void		ft_err_cmd_not_found(t_msh_command *cmd)
 {
 	ft_print_err_head("Pro tip: type a valid command!\n\n");
 	ft_putstr_fd("minishell: command not found: ", 2);
 	ft_putstr_fd(cmd->prog_name, 2);
 	ft_putchar_fd(2, '\n');
+}
+
+/*
+** Search in st_mode if passed program is a directory or non executable
+*/
+
+void		ft_handle_err_code(int err, char *prog_name)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(prog_name, 2);
+	if (err == 1)
+		ft_putstr_fd(": is a directory\n", 2);
+	else if (err == 2)
+		ft_putstr_fd(": permission denied\n", 2);
+}
+
+int			ft_handle_err(t_msh_command *cmd)
+{
+	int		err;
+
+	err = 0;
+	if (S_ISDIR(cmd->prog_stats->st_mode))
+		ft_handle_err_code((err = 1), cmd->prog_name);
+	else if (!(cmd->prog_stats->st_mode & S_IXUSR))
+		ft_handle_err_code((err = 2), cmd->prog_name);
+	return ((err == 0) ? (0) : (1));
 }
