c97b29edbdcc56d9ea8d959137e6c0f716731aaa
Joffrey Janiec <jjaniec@student.42.fr>
2018-04-11 16:28:12 +0200

    add srcs/ft_msh_setenv.c - working setenv function

--- srcs/ft_exec_builtin.c
+++ srcs/ft_exec_builtin.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 17:31:48 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/04/10 22:44:10 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/04/11 16:18:43 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,4 +46,8 @@ void			ft_exec_builtin(int blt, t_msh_command *cmd)
 		ft_exec_builtin_env();
 	else if (blt == 2)
 		ft_exec_builtin_cd(cmd);
+	else if (blt == 3 && cmd->prog_prms[1] && cmd->prog_prms[2])
+		ft_msh_setenv(cmd->prog_prms[1], cmd->prog_prms[2], 1);
+	/*else if (blt == 4)
+		ft_exec_builtin_unsetenv(cmd);*/
 }
