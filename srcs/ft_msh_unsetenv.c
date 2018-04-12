182cb4d46446a208479f2e10c2823a0d9945bd5d
Joffrey Janiec <jjaniec@student.42.fr>
2018-04-12 22:07:09 +0200

    fix invalid write fix unsetenv - fix ft_print_prompt crash without HOME value

--- srcs/ft_msh_unsetenv.c
+++ srcs/ft_msh_unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/11 18:06:47 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/04/11 18:31:27 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/04/12 22:04:45 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ void			ft_msh_unsetenv(char *name)
 	{
 		while (g_msh_params->cur_environ[++i])
 			;
-		new_env = malloc(sizeof(char *) * i);
+		new_env = malloc(sizeof(char *) * (i + 1));
 		new_env[i] = NULL;
 		i = -1;
 		j = 0;
--- srcs/ft_print_prompt.c
+++ srcs/ft_print_prompt.c
@@ -6,16 +6,28 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/05 15:33:19 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/04/10 18:53:43 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/04/12 22:05:56 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-static void	ft_print_cur_dir(char *cwd, char *home)
+static void	ft_print_cur_dir(char *cwd)
 {
-	if (ft_strstr(cwd, home))
-		PRINTF("\e[34m~%s\e[39m\n", cwd + ft_strlen(home));
+	char	*cur_path_fmt;
+	char	*new_path_begin;
+	int		i;
+
+	new_path_begin = NULL;
+	i = -1;
+	cur_path_fmt = ft_get_path_var_val(g_msh_params->cur_environ, "PWD");
+	if (cur_path_fmt && g_msh_params->home_fmt && \
+		ft_strstr(cur_path_fmt, g_msh_params->home_fmt))
+	{
+		while (cur_path_fmt[i] == g_msh_params->home_fmt[i])
+			i++;
+		PRINTF("\e[34m~%s\e[39m\n", cur_path_fmt + i);
+	}
 	else
 		PRINTF("\e[34m%s\e[39m\n", cwd);
 }
@@ -27,6 +39,6 @@ static void	ft_print_cur_dir(char *cwd, char *home)
 void		ft_print_prompt(void)
 {
 	write(1, "\n", 1);
-	ft_print_cur_dir(g_msh_params->cwd_fmt, g_msh_params->home_fmt);
+	ft_print_cur_dir(g_msh_params->cwd_fmt);
 	write(1, "❯ ", 4);
 }
